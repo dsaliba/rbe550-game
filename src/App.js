37413fe0131dec9f4ea4233803c5903b91c79e91
Dimitri Saliba <dimitriasaliba@gmail.com>
2025-01-28 22:00:52 -0500

    p5 test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { ReactP5Wrapper } from "@p5-wrapper/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function sketch(p5) {
+  let clicks = [];
+  let img; 
+
+  p5.preload = () => {
+    img = p5.loadImage("/city.jpg"); 
+  };
+
+  p5.setup = () => {
+    p5.createCanvas(1280, 720, p5.WEBGL)
+    console.log(p5);
+
+  };
+
+  p5.draw = () => {
+    p5.background(250);
+    if (img) {
+      p5.image(img, 50, 50, 300, 300);
+    }
+    clicks.forEach((click)=> {
+      p5.ellipse(click.x, click.y, 20, 20);
+    })
+  };
+
+  p5.mouseClicked = () => {
+    console.log('Mouse clicked at:', p5.mouseX, p5.mouseY);
+    clicks.push({x:p5.mouseX - (p5.width/2), y:p5.mouseY - p5.height/2});
+    
+  };
+
+  
+
+
 }
 
-export default App;
+export default function App() {
+  return <ReactP5Wrapper sketch={sketch} />;
+}
