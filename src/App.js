db5dde00e471b5a269f41ea1c83cd23d32e4bac6
Dimitri Saliba <dimitriasaliba@gmail.com>
2025-02-10 23:19:24 -0500

    starting to outline project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import * as React from "react";
+import { useReducer} from "react";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 import matrix from "./Matrix";
+
+let grid;
+let freeTiles = [];
+
+let currentEvent = undefined;
+let forceUpdate = ()=>undefined;
+
 
 function sketch(p5) {
 
@@ -8,29 +16,112 @@ function sketch(p5) {
   const tileSize = 10;
   let rows;
   let cols;
-  let grid;
+  
   let botSpeed = 2;
   let dragStart = null;
   let currentSelections = [];
+  let imgSize = {w:1200, h:870};
+  let events = [];
+  let timer = 0;
 
   p5.preload = () => {
-    img = p5.loadImage("/campus.jpg"); 
+    img = p5.loadImage("/bigcity.jpg"); 
+    grid= matrix;
+    grid.forEach((row, r) => {
+      row.forEach((type, c)=> {
+        if (type === 0) {
+          freeTiles.push({r:r, c:c});
+        }
+      });
+    });
+    
+    
     
   }
   p5.setup = () => {
-    p5.createCanvas(1570  , 1240, p5.WEBGL)
-    rows = Math.floor(p5.width/tileSize);
-    cols = Math.floor(p5.height/tileSize);
-    //grid = Array.from(Array(rows), () => new Array(cols).fill(0));
-    grid= matrix;
+    p5.createCanvas(imgSize.w  , imgSize.h, p5.WEBGL)
+    rows = Math.floor(imgSize.w/tileSize);
+    cols = Math.floor(imgSize.h/tileSize);
+    //grid = Array.from(Array(rows), () => new Array(cols).fill(1));
+    
     console.log(p5);
     console.log(grid);
+    setTimeout(spawnRandomEvent, 2000);
   };
 
+  function getRandomBoringTile() {
+    let invalid = true;
+    let tile;
+    while (invalid) { //spooky
+      tile = freeTiles[Math.floor(Math.random()*freeTiles.length)];
+      invalid = false;
+      events.forEach((event)=> {
+        if (Math.abs(event.r -tile.r) + Math.abs(event.c-tile.c) < 20) {
+          invalid = true;
+          return;
+        }
+      })
+    }
+    return tile;
+  }
+
+
+  function spawnRandomEvent() {
+    let spawnTile = getRandomBoringTile();
+    switch(Math.floor(Math.random()*4)) {
+      case 0:
+        events.push({
+          color: [247, 118, 142],
+          flash: true,
+          r: spawnTile.r,
+          c: spawnTile.c,
+          title: "Misparked Car"
+
+        })
+      break;
+      case 1:
+        events.push({
+          color: [247, 118, 142],
+          flash: true,
+          r: spawnTile.r,
+          c: spawnTile.c,
+          title: "Faulty fusebox"
+        })
+      break;
+
+      case 2:
+        events.push({
+          color: [247, 118, 142],
+          flash: true,
+          r: spawnTile.r,
+          c: spawnTile.c,
+          title: "Cat stuck in tree"
+        })
+      break;
+
+      case 3:
+        events.push({
+          color: [247, 118, 142],
+          flash: true,
+          r: spawnTile.r,
+          c: spawnTile.c,
+          title: "Man passed out"
+        })
+      break;
+    }
+    console.log("event spawned");
+    setTimeout(spawnRandomEvent, 10000 + Math.random()*5000);
+  }
+
+
   p5.draw = () => {
+    
+    timer ++;
+    
+
     p5.background(250);
     if (img) {
-      p5.image(img, -p5.width/2, -p5.height/2, p5.width, p5.height);
+      p5.image(img, -p5.width/2, -p5.height/2, imgSize.w, imgSize.h);
     }
 
     let x1
@@ -45,30 +136,30 @@ function sketch(p5) {
     }
     
     p5.rectMode(p5.CORNERS);
-    p5.stroke(0, 0, 0, 100);
+    p5.stroke(0, 0, 0, 10);
     grid.forEach((row, r) => {
       row.forEach((type, c)=> {
-        p5.fill(grid[r][c]===1?255:0, 0, 0, grid[r][c]===1?100:0);
-        if (dragStart !== null) {
-          let tx = r*tileSize - p5.width/2 + tileSize/2;
-          let ty = c*tileSize - p5.height/2 + tileSize/2;
-          if (tx > x1 && tx < x2) {
-            if (ty > y1 && ty < y2) {
-              p5.fill(255, 0, 255, 100);
-              currentSelections.push({r: r, c: c});
-            }
-          }
-        }
-        
-        p5.rect(r*tileSize - p5.width/2, c*tileSize - p5.height/2, (r+1)*tileSize - p5.width/2, (c+1)*tileSize - p5.height/2);
+        p5.fill(grid[r][c]===1?255:0, 0, 0, grid[r][c]===1?50:0);
+        //p5.rect(r*tileSize - p5.width/2, c*tileSize - p5.height/2, (r+1)*tileSize - p5.width/2, (c+1)*tileSize - p5.height/2);
+      
       });
     });
 
-    if (dragStart !== null) {
-      p5.fill(0, 0, 255, 100);
-      p5.stroke(0, 0, 0);
-      p5.rect(dragStart.x, dragStart.y, p5.mouseX - p5.width/2, p5.mouseY - p5.height/2);
-    }
+    events.forEach((event)=> {
+      p5.fill(event.color[0], event.color[1], event.color[2]);
+      if (event.flash && timer%30 > 15) p5.fill(255);
+      p5.stroke(0);
+      p5.strokeWeight(2);
+      if (currentEvent === event) {
+        p5.stroke(42, 195, 222);
+        p5.strokeWeight(3);
+      }
+      
+      p5.ellipseMode(p5.RADIUS);
+      p5.ellipse(event.r*tileSize - p5.width/2 + tileSize/2, event.c*tileSize - p5.height/2 + tileSize/2, tileSize, tileSize);
+    })
+
+
 
 
    
@@ -77,7 +168,21 @@ function sketch(p5) {
 
   p5.mouseClicked = () => {
     console.log('Mouse clicked at:', p5.mouseX, p5.mouseY);
-    //clicks.push({x:p5.mouseX - (p5.width/2), y:p5.mouseY - p5.height/2});
+    let x = p5.mouseX ;
+    let y = p5.mouseY;
+    events.forEach((event)=> {
+      let ex = event.r*tileSize + tileSize/2;
+      let ey = event.c*tileSize + tileSize/2;
+      if (((ex-x)*(ex-x))+((ey-y)*(ey-y)) < tileSize*tileSize) {
+        currentEvent = event;
+        event.flash = false;
+        forceUpdate();
+
+      }
+    });
+    
+    
+    
     
  
   };
@@ -89,7 +194,7 @@ function sketch(p5) {
   p5.mouseReleased = () => {
     dragStart = null;
     currentSelections.forEach((pair)=> {
-      grid[pair.r][pair.c] = 1;
+      grid[pair.r][pair.c] = 0;
     })
     printMatrixAsDeclaration(grid);
 
@@ -123,5 +228,29 @@ function printMatrixAsDeclaration(matrix) {
 }
 
 export default function App() {
-  return <ReactP5Wrapper sketch={sketch} />;
+  const [, forceUpdateLocal] = useReducer(x => x + 1, 0);
+  forceUpdate = forceUpdateLocal;
+
+  return <div className="outer-box">
+    <div className="game-box">
+      <div className="sketch-box">
+      <ReactP5Wrapper sketch={sketch} />
+      </div>
+      <div className="side-panel">
+        {currentEvent===undefined?"":((()=>{
+          return <div className="event">
+              <h1 className="event-title">{currentEvent.title}</h1>
+              <div className="event-description">{currentEvent.description}</div>
+              <div className="event-button-box">
+                <button className="cop-button">Send Police</button>
+                <button className="fire-button">Send Firefighters</button>
+                <button className="ems-button">Send EMS</button>
+                <button className="main-button">Send Maintance</button>
+              </div>
+            </div>
+        }))()}
+      </div>
+    </div>
+
+  </div>
 }
